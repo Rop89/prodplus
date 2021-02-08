2e8e89ce81452c0799314e85bf722a5ef1b9020b
Rpx <ritaa.pinho@gmail.com>
2021-02-08 07:37:17 +0000

    first commit

---
+++ src/components/NavbarButton.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Grommet, Box, Button, DropButton, Heading, Text } from 'grommet';
+import PropTypes from 'prop-types';
+import { Close } from 'grommet-icons';
+import { grommet } from 'grommet/themes';
+
+const DropContent = ({ onClose }) => (
+  <Box pad="small">
+    <Box direction="row" justify="between" align="center">
+      <Heading level={3} margin="small">
+        Heading
+      </Heading>
+      <Button icon={<Close />} onClick={onClose} />
+    </Box>
+    <Text>Content</Text>
+  </Box>
+);
+
+DropContent.propTypes = {
+  onClose: PropTypes.func.isRequired,
+};
+
+const NavbarButton = () => {
+  const [open, setOpen] = React.useState();
+  const onOpen = () => {
+    setOpen(true);
+  };
+  const onClose = () => {
+    setOpen(false);
+  };
+
+
+  return (
+    <Grommet theme={grommet}>
+      <Box 
+      align="center" 
+      pad="large">
+        <DropButton
+          label="Open"
+          open={open}
+          onOpen={onOpen}
+          onClose={onClose}
+          dropContent={<DropContent onClose={onClose} />}
+          dropProps={{ align: { top: 'bottom' } }}
+        />
+      </Box>
+    </Grommet>
+  );
+  }
+
+
+export const Simple = () => <NavbarButton />;
+Simple.story = {
+  parameters: {
+    chromatic: { disable: true },
+  },
+};
+
+
+export default NavbarButton;
---
+++ src/views/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Navbar"
+
+
+function Home() {
+  return (
+    <div className="font-sans">
+      <Navbar/>
+    </div>
+ 
+
+  );
+}
+export default Home;
